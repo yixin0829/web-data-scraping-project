3aaeef2382d46e69f25e0e6af758bb6b898872ac
yixin0829 <yixin0829@outlook.com>
2019-12-23 21:59:10 -0600

    soup half-done

--- web_scraping.py
+++ web_scraping.py
@@ -2,8 +2,21 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://ags.aer.ca/data-maps-models/digital-data').text
+source = requests.get('https://ags.aer.ca/data-maps-models/digital-data')
+content = source.content
 
-soup = BeautifulSoup(source, features='lxml')
+# Creating a soup object
+soup = BeautifulSoup(content, features='lxml')
 
-print(soup)
+result = soup.find(class_ = "list-results row")
+result_rows = result.find_all(class_ = "unstyled item-content")
+
+# Extracting all the dates data and append them to result_list
+result_list = []
+for element in result_rows:
+    li_tag = element.find("li")
+    result_list.append(li_tag.get_text())
+
+print(result_list)
+
+

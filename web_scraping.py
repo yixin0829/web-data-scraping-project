f3127a114c52ec7be333320fcab3759e2655c832
yixin0829 <yixin0829@outlook.com>
2019-12-30 11:28:09 -0600

    add dev branch

--- web_scraping.py
+++ web_scraping.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
-import nltk
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+
+
 
 source = requests.get('https://ags.aer.ca/data-maps-models/digital-data')
 content = source.content
@@ -14,18 +17,19 @@ soup = BeautifulSoup(content, features='lxml')
 # Narrowing down the searching scope
 result = soup.find(class_ = "list-results row")
 # html black that contains result of year & id, title, and author data
-result_rows = result.find_all(class_ = "unstyled item-content")
+result_blocks = result.find_all(class_ = "unstyled item-content")
 # html block that contains the publication date data
 date_blocks = result.find_all(class_ = "pub-date")
+# html list block that contains author data
+author_blocks = result.find_all(class_ = "author-list")
+
 
 # Extracting all dates, titles, and author names
 # Append the data to separate lists
 year_list = []
-id_list = []
-for element in result_rows:
+for element in result_blocks:
     li_tag = element.find("li")
     year_list.append(li_tag.get_text()[13:17])
-    id_list.append(li_tag.get_text()[18:22])
 
 publication_date_list = []
 for element in date_blocks:
@@ -33,20 +37,23 @@ for element in date_blocks:
     publication_date_list.append(element.get_text().strip()) 
 
 title_list = []
-for element in result_rows:
+for element in result_blocks:
     a_tag = element.find("a")
     title_list.append(a_tag.get_text())
 
 author_list = []
-for element in result_rows:
-    a_tag = element.find_all("a")[1]
-    author_list.append(a_tag.get_text())
+for element in author_blocks:
+    a_tag = element.find_all("a")
+    # Append the author names into the list depending on the lenth of the a_tag list (i.e. how many authors)
+    for x in range(len(a_tag)):
+        author_list.append(a_tag[x].get_text())
+
 
 # Extract the abstract for each article (needs a second get request)
 # Issue: take too much time ~25 min
 """
 abstract_list = []
-for element in result_rows:
+for element in result_blocks:
     a_tag = element.find("a")
     link = a_tag.attrs['href']
     article = requests.get('https://ags.aer.ca/'+ link)
@@ -61,7 +68,6 @@ for element in result_rows:
 """   
 
 # print(year_list)
-# print(id_list)
 # print(title_list)
 # print(author_list)
 # print(publication_date_list)
@@ -73,14 +79,17 @@ n, bins, patches = plt.hist(year_list, num_bins, facecolor='blue', alpha=0.5)
 plt.xlabel('Years')
 plt.ylabel('# of Sets')
 plt.title(r'Number of Published Data Sets from 2002 - 2019')
-plt.subplots_adjust(left=0.15)
-plt.show()
 
-# nltk.download('punkt')
-# def graph():
-#   tokens = nltk.tokenize.word_tokenize(title_list)
-#   fd = nltk.FreqDist(tokens)
-#   fd_t10 = fd.most_common(10)
-#   fd.plot(30,cumulative=False)
 
-# graph()
+
+
+# Creating a new plot for visualizing the popular publishers
+plt.figure()
+# Declaring fdist object of class FreqDist
+fdist = FreqDist()
+for word in author_list:
+    fdist[word.lower()] += 1
+
+plt.title(r'Number of Published Data Sets from Different Publishers')
+fdist.plot(20)
+
